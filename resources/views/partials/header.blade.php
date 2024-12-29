afbc8be191a3598faf28a378803fd7e6263a8412
Tayfun <57490629+tafu18@users.noreply.github.com>
2024-12-29 11:17:21 +0300

    Add real-time date and time display to header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -12,7 +12,35 @@
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{ route('projects') }}">Projelerim</a></li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{ route('posts') }}">Gönderiler</a></li>
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="{{ route('contact') }}">İletişim</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" id="datetime" href="{{ route('home') }}"></a></li>
             </ul>
         </div>
     </div>
+    <!-- Right: Date and Time -->
+    <div class="text-right text-white text-font" id="datetime" style="font-size: 1rem; font-weight: 500; padding-left: 10rem; padding-right:5rem"></div>
+    </div>
 </nav>
+
+<script>
+    function updateDateTime() {
+        const now = new Date();
+        now.setHours(now.getHours());
+
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false
+        };
+
+        const dateTimeString = now.toLocaleString('tr-TR', options);
+        document.getElementById('datetime').textContent = dateTimeString;
+    }
+
+    setInterval(updateDateTime, 1000);
+    updateDateTime();
+</script>
